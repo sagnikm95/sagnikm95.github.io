3e5c451304de1dc20a186700078d64b9ef4751f0
Sagnik M <sagnikm.ju@gmail.com>
2017-01-12 11:27:57 +0530

    First Commit

--- scripts/main.js
+++ scripts/main.js
@@ -11,32 +11,36 @@ function Velocity(v_x, v_y) {
 	this.y = v_y;
 }
 
-var BALL_RADIUS = 5;
+var BALL_RADIUS = 10;
 
 function Ball(pos, vel) {
 	this.pos = pos;
 	this.vel = vel;
 
 	this.draw = function() {
-		context.fillStyle = "white";
-		context.fillRect(
+		
+		/*context.fillRect(
 				this.pos.x - BALL_RADIUS, this.pos.y - BALL_RADIUS,
-				2 * BALL_RADIUS, 2 * BALL_RADIUS);
+				2 * BALL_RADIUS, 2 * BALL_RADIUS);*/
+		context.arc(this.pos.x,this.pos.y,BALL_RADIUS,0,2*Math.PI)
+		context.fillStyle = "white";		;
+		context.fill();
 	}
 
-	this.move = function(dt) {
-		if (this.pos.x + BALL_RADIUS < 0 ||
-				this.pos.x - BALL_RADIUS >= canvas.width) {
+	this.move = function() {
+		if (this.pos.x +this.vel.x- BALL_RADIUS <0 ||
+				this.pos.x +this.vel.x + BALL_RADIUS >= canvas.width) {
+			//this.vel.x=-this.vel.x;
+			//this.pos.x+=this.vel.x * dt;
 			return false;
 		}
 
-		if (this.pos.y - BALL_RADIUS <= 0 && this.vel.y < 0 ||
-				this.pos.y + BALL_RADIUS >= canvas.height - 1 &&
-				this.vel.y > 0) {
+		if (this.pos.y + this.vel.y - BALL_RADIUS <0 ||
+				this.pos.y + this.vel.y + BALL_RADIUS > canvas.height  ) {
 			this.vel.y = -this.vel.y;
 		}
-		this.pos.x += this.vel.x * dt;
-		this.pos.y += this.vel.y * dt;
+		this.pos.x += this.vel.x ;
+		this.pos.y += this.vel.y ;
 
 		return true;
 	}
@@ -73,29 +77,35 @@ function draw_board() {
 	context.closePath();
 }
 
-var BALL_START_POS = new Pos(canvas.width / 2, BALL_RADIUS);
-var PADDLE1_START_POS = new Pos(9, canvas.height / 2);
+var BALL_START_POS = new Pos(canvas.width / 2, BALL_RADIUS); //We are maintaing the centre of the balll in {pos}
+var PADDLE1_START_POS = new Pos(9, canvas.height / 2); // We are maintaining the centre of the rectangle in { pos}
 var PADDLE2_START_POS = new Pos(canvas.width - 1 - 9, canvas.height / 2);
 
-var ball = new Ball(BALL_START_POS, new Velocity(-5, 20));
+var ball = new Ball(BALL_START_POS, new Velocity(-5, 5));
 var paddle1 = new Paddle(PADDLE1_START_POS);
 var paddle2 = new Paddle(PADDLE2_START_POS);
-var lastDrawnTime = new Date().getTime();
+//var lastDrawnTime = new Date().getTime();
 
 function drawGameState() {
 	draw_board();
 	ball.draw();
 	paddle1.draw();
 	paddle2.draw();
+	//lastDrawnTime = new Date().getTime();
 }
 
 window.main = function() {
 	window.requestAnimationFrame(main);
-	var now = new Date().getTime();
-	if (ball.move((now - lastDrawnTime) / 1000)) {
+	//var now = new Date().getTime();
+	if (ball.move()) {
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		drawGameState();
 	}
+	else{
+		alert("LOST GAME OVER");
+		document.location.reload();
+	}
+	
 }
 
 main();
